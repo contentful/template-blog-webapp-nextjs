a35e059c0ebabd77477664baa84a98dc27867451
Matt van Voorst <mattvanvoorst@contentful.com>
2022-12-08 17:29:10 +0100

    feat: [PLATO-307] data integration - Added graphql-codegen - Successfully generated the schema files and committed them - Added all the base GraphQl files, wrote queries for them - Set up the basic data fetching for the landing and the blog detail pages

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,6 @@
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 
-
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <main>
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,18 +1,40 @@
-import { InferGetStaticPropsType } from 'next';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-import { getServerSideTranslations } from "./utils/get-serverside-translations";
+import { getServerSideTranslations } from './utils/get-serverside-translations';
 
-const Page = ({ }: InferGetStaticPropsType<typeof getStaticProps>) => {
-    return <h1>Hello world!</h1>;
+import { client } from '@src/lib/client';
+import { revalidateDuration } from '@src/pages/utils/constants';
+
+const Page = ({ page }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return <h1>{page.featuredBlogPost.title}</h1>;
 };
 
-export const getStaticProps = async ({ locale }) => {
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  try {
+    const data = await client.pageLanding({ locale });
+
+    const page = data.pageLandingCollection?.items[0];
+
+    if (!page) {
+      return {
+        revalidate: revalidateDuration,
+        notFound: true,
+      };
+    }
+
     return {
-        revalidate: 60,
-        props: {
-            ...(await getServerSideTranslations(locale)),
-        }
+      props: {
+        revalidate: revalidateDuration,
+        ...(await getServerSideTranslations(locale)),
+        page,
+      },
     };
+  } catch {
+    return {
+      revalidate: revalidateDuration,
+      notFound: true,
+    };
+  }
 };
 
 export default Page;

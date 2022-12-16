d047b49a4471d1b3e8c0d00a3d907685cf80b968
Nkoyo Ating <atingenkay@gmail.com>
2022-12-16 17:31:05 +0100

    PLATO-321: add accessibilty for mobile language selector

--- src/components/language-selector/LanguageSelectorMobile.tsx
+++ src/components/language-selector/LanguageSelectorMobile.tsx
@@ -12,50 +12,47 @@ export const LanguageSelectorMobile = ({ localeName, displayName }) => {
   const [showDrawer, setShowDrawer] = useState(false);
 
   return (
-    <FocusLock>
+    <>
       {showDrawer ? (
-        <span
+        <button
           className="fixed right-10 top-4 z-50 flex cursor-pointer items-center"
-          onClick={() => setShowDrawer(!showDrawer)}
-          role="button"
-          tabIndex={0}>
+          onClick={() => setShowDrawer(!showDrawer)}>
           <CloseIcon width="18px" height="18px" variant="secondary" />
-        </span>
+        </button>
       ) : (
-        <span
+        <button
           onClick={() => setShowDrawer(!showDrawer)}
-          role="button"
-          tabIndex={0}
           className="fixed right-10 top-6 z-30 flex cursor-pointer items-center">
           <LanguageIcon width="18px" height="18px" variant="secondary" />
-        </span>
+        </button>
       )}
-
-      <div
-        className={twMerge(
-          `fixed top-0 right-0 z-40 h-full w-[80vw] bg-colorWhite pt-5 pl-5 duration-300 ease-in-out`,
-          showDrawer ? 'translate-x-0' : 'translate-x-full',
-        )}>
-        <h2 className="py-4 text-xl font-semibold">{t('common.regionalSettings')}</h2>
-        <>
-          <p className="pb-2 text-base font-semibold text-colorBlack"> {t('common.language')}</p>
-          <select
-            className="block w-10/12 rounded-md border border-gray300 py-2 pl-4 pr-8 text-sm"
-            defaultValue={locale}
-            onChange={event => {
-              router.push({ pathname: router.pathname, query: router.query }, router.asPath, {
-                locale: String(event.target.value),
-              });
-              setShowDrawer(!showDrawer);
-            }}>
-            {locales?.map(availableLocale => (
-              <option key={availableLocale} value={availableLocale}>
-                {displayName(availableLocale).of(localeName(availableLocale))}
-              </option>
-            ))}
-          </select>
-        </>
-      </div>
-    </FocusLock>
+      <FocusLock disabled={!showDrawer} returnFocus={true}>
+        <div
+          className={twMerge(
+            `fixed top-0 right-0 z-40 h-full w-[80vw] bg-colorWhite pt-5 pl-5 duration-300 ease-in-out`,
+            showDrawer ? 'translate-x-0' : 'translate-x-full',
+          )}>
+          <h2 className="py-4 text-xl font-semibold">{t('common.regionalSettings')}</h2>
+          <>
+            <p className="pb-2 text-base font-semibold text-colorBlack"> {t('common.language')}</p>
+            <select
+              className="block w-10/12 rounded-md border border-gray300 py-2 pl-4 pr-8 text-sm"
+              defaultValue={locale}
+              onChange={event => {
+                router.push({ pathname: router.pathname, query: router.query }, router.asPath, {
+                  locale: String(event.target.value),
+                });
+                setShowDrawer(!showDrawer);
+              }}>
+              {locales?.map(availableLocale => (
+                <option key={availableLocale} value={availableLocale}>
+                  {displayName(availableLocale).of(localeName(availableLocale))}
+                </option>
+              ))}
+            </select>
+          </>
+        </div>
+      </FocusLock>
+    </>
   );
 };

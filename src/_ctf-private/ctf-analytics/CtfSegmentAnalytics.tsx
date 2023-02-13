7f39314da2555f859987ca7c16161c9a84dedb4b
Matt van Voorst <mattvanvoorst@contentful.com>
2023-02-13 11:52:02 +0100

    feat: [PLATO-409] Segment integration - Added initialisation check

--- src/_ctf-private/ctf-analytics/CtfSegmentAnalytics.tsx
+++ src/_ctf-private/ctf-analytics/CtfSegmentAnalytics.tsx
@@ -6,7 +6,7 @@ import { useCtfConsent } from '@src/_ctf-private';
 import typewriter from 'analytics';
 
 export const CtfSegmentAnalytics = () => {
-  useCtfConsent();
+  const { data } = useCtfConsent();
 
   useEffect(() => {
     typewriter.setTypewriterOptions({
@@ -30,7 +30,7 @@ export const CtfSegmentAnalytics = () => {
     });
   }, []);
 
-  return (
+  return data?.isInitialized ? (
     <Script id="segment-tracking-script">
       {`
           !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${process.env.NEXT_PUBLIC_SEGMENT_WRITEKEY}";;analytics.SNIPPET_VERSION="4.15.3";
@@ -39,5 +39,5 @@ export const CtfSegmentAnalytics = () => {
           }}();
         `}
     </Script>
-  );
+  ) : null;
 };

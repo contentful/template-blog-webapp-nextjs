736dd7eb74a925963d1299bf9fcd353dbb441a2d
Matt van Voorst <mattvanvoorst@contentful.com>
2023-02-21 16:57:13 +0100

    feat: [PLATO-411] Tracking events - Additional nullcheck

--- src/_ctf-private/ctf-analytics/CtfSegmentAnalytics.tsx
+++ src/_ctf-private/ctf-analytics/CtfSegmentAnalytics.tsx
@@ -22,13 +22,15 @@ export const CtfSegmentAnalytics = () => {
   });
 
   const handleRouteChange = useCallback(() => {
-    analytics.page({
-      templateId: 'blog',
-      xRayActive: xray,
-      previewActive: preview,
-      guestSpaceActive,
-    });
-  }, [guestSpaceActive, preview, xray]);
+    if (initialized && analytics) {
+      analytics?.page({
+        templateId: 'blog',
+        xRayActive: xray,
+        previewActive: preview,
+        guestSpaceActive,
+      });
+    }
+  }, [guestSpaceActive, initialized, preview, xray]);
 
   useEffect(() => {
     if (guestSpaceActive && initialized) {

e31cbe7c0b7ce3a51490ec0484fc3ed372f4212f
Nkoyo Ating <atingenkay@gmail.com>
2023-05-10 10:43:48 +0200

    PLATO-620: fix signup banner sticky position

--- src/_ctf-private/ctf-sign-up-banner/CtfSignUpBanner.tsx
+++ src/_ctf-private/ctf-sign-up-banner/CtfSignUpBanner.tsx
@@ -11,7 +11,7 @@ export const CtfSignUpBanner = () => {
   const handleSignUpLinkClick = () => typewriter.signUpBannerInteracted({ ctaClicked: true });
 
   return (
-    <div className="sticky flex h-[54px] items-center justify-center gap-3 bg-[#0033A3]">
+    <div className="sticky top-0 z-[200] flex h-[54px] items-center justify-center gap-3 bg-[#0033A3]">
       <p className="block font-systemUIFontFamilies text-sm text-colorWhite md:hidden">
         Content managed via
       </p>
@@ -19,17 +19,15 @@ export const CtfSignUpBanner = () => {
       <p className="hidden font-systemUIFontFamilies text-sm text-colorWhite md:block">
         The content on this template is managed via Contentful
       </p>
-      <Link
-        className="rounded-[50px] bg-[#FFDA00] py-2 px-6 text-[#464E5B] transition-transform duration-300 ease-in hover:bg-[#EFC800]"
-        href={signUpLink}
-        target="_blank"
-        onClick={handleSignUpLinkClick}>
-        <p className="block font-systemUIFontFamilies text-sm font-semibold  md:hidden">
-          Use template
-        </p>
-        <p className="hidden font-systemUIFontFamilies text-sm font-semibold  md:block ">
-          Start with this template
-        </p>
+      <Link href={signUpLink} target="_blank" onClick={handleSignUpLinkClick}>
+        <button className="rounded-[50px] bg-[#FFDA00] py-2 px-6 text-[#464E5B] transition-transform duration-[400ms] ease-in hover:bg-[#EFC800]">
+          <p className="block font-systemUIFontFamilies text-sm font-semibold  md:hidden">
+            Use template
+          </p>
+          <p className="hidden font-systemUIFontFamilies text-sm font-semibold  md:block ">
+            Start with this template
+          </p>
+        </button>
       </Link>
     </div>
   );

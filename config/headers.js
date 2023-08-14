1086ad0128dc913b37e0d717846681e306dbf5b4
Eddie Chung <47013507+roosterhack@users.noreply.github.com>
2023-08-14 10:36:09 +0200

    fix: [PLATO-842] Add eu domain to images optimization and CSP (#103)

--- config/headers.js
+++ config/headers.js
@@ -13,7 +13,7 @@ const securityHeaders = [
   },
   {
     key: 'Content-Security-Policy',
-    value: `frame-ancestors 'self' https://app.contentful.com`,
+    value: `frame-ancestors 'self' https://app.contentful.com https://app.eu.contentful.com`,
   },
   {
     key: 'X-Content-Type-Options',
